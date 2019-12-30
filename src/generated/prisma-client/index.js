59f3c7b5b2a3f3c1385b47fb72c1d80bba58bed7
molly <molly.zhou@rea-group.com>
2019-12-30 17:17:39 +1100

    pai again

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -25,6 +25,6 @@ exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
   endpoint: `https://melbookshare-a60a64c5ac.herokuapp.com/api/v1`,
-  secret: `${process.env["PRISMA_SECRET"]}`
+  secret: "EVsZxKMUpJE5m7rnpVXyR8DjIALToa0F"
 });
 exports.prisma = new exports.Prisma();
